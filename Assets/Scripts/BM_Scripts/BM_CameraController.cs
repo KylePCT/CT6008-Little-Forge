39c34077621373ce7d5f6782e9581507fb25352d
BlondyMartinez <blondymartinezm@gmail.com>
2020-02-27 18:04:35 +0000

    character controller
    jump, movement, aiming (still needs to be worked on)

---
+++ Assets/Scripts/BM_Scripts/BM_CameraController.cs
@@ -0,0 +1,34 @@
+﻿
+using UnityEngine;
+
+public class BM_CameraController : MonoBehaviour {
+    private Transform player;
+    public Vector3 normalOffset, aimingOffset;
+    private bool offsetChanged = false;
+    private bool angleChanged = false;
+
+    private void Start() {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        transform.localPosition = normalOffset;
+    }
+
+    private void Update() {
+        if (BM_CharacterController.isAiming) {
+            transform.localPosition = aimingOffset;
+            if (!angleChanged) {
+                angleChanged = true;
+                transform.localEulerAngles = new Vector3(0, 0, 0);
+            }
+            offsetChanged = false;
+            Camera.main.fieldOfView = 40;
+        } else {
+            if (!offsetChanged) {
+                transform.localPosition = normalOffset;
+                offsetChanged = true;
+                angleChanged = false;
+            }
+            Camera.main.fieldOfView = 60;
+            transform.LookAt(player);
+        }
+    }
+}
---
+++ Assets/Scripts/BM_Scripts/BM_CameraParent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BM_CameraParent : MonoBehaviour {
+
+    public float mouseRotationSpeed;
+    private float yaw = 0.0f, pitch = 0.0f;
+
+    public float minPitch, maxPitch;
+
+    private Transform player;
+    // Start is called before the first frame update
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!BM_CharacterController.isAiming) {
+            yaw += mouseRotationSpeed * Input.GetAxis("Mouse X");
+            pitch -= mouseRotationSpeed * Input.GetAxis("Mouse Y");
+
+            pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
+
+            transform.eulerAngles = new Vector3(pitch, yaw, 0);
+        } else {
+            Vector3 temp = transform.rotation.eulerAngles;
+            transform.eulerAngles = new Vector3(0, temp.y, 0);
+        }
+
+        transform.position = new Vector3 (player.position.x, transform.position.y, player.position.z);
+    }
+}
