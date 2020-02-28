e62e8be9357a52812df62131a7088da230215cd7
Sam Baker <s1704633@connect.glos.ac.uk>
2020-02-28 00:11:44 +0000

    Currency added

---
+++ Assets/Scripts/SB_Scripts/NewGame/PlayersBank.cs
@@ -0,0 +1,98 @@
+﻿//////////////////////////////////////////////////
+/// File: PlayersBank.cs
+/// Author: Sam Baker
+/// Date created: 27/02/20
+/// Last edit: 27/02/20
+/// Description: Singleton, PlayerBanks is used to manage the player currency. The PlayersBank instance
+///         can be called and a set of functions are available to modifiy the currency.
+/// Comments: This script needs to be attatched to an object in the starting scene.
+///         *THE CURRENY IS A FLOAT, was an int but not anymore ;)*
+//////////////////////////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersBank : MonoBehaviour
+{
+    [Header("_____________________________________________________")]
+    [Space(-20)]
+    [Header(" > PlayersBank.Instance.GetMoney();")]
+    [Space(-10)]
+    [Header(" > PlayersBank.Instance.SetMoney(float);")]
+    [Space(-10)]
+    [Header(" > PlayersBank.Instance.ResetMoney();")]
+    [Space(-10)]
+    [Header(" > PlayersBank.Instance.TakeAwayMoney(float);")]
+    [Space(-10)]
+    [Header(" > PlayersBank.Instance.AddMoney(float);")]
+    [Space(-10)]
+    [Header("PlayersBank's functions")]
+    //////////////////////////////////////////////////
+    //// Variables
+    [SerializeField] private float m_playersMoney = 0;
+    private static PlayersBank m_instance;
+    public static PlayersBank Instance { get { return m_instance; } }
+
+    //////////////////////////////////////////////////
+    //// Functions
+    private void Awake()
+    {
+        if (m_instance != null && m_instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            m_instance = this;
+        }
+    }
+    /// <summary>
+    /// Function inside the PlayersBank.cs, used to increase the amount of money in the players bank.
+    /// </summary>
+    /// <param name="a_money">Amount to add to the players bank</param>
+    public void AddMoney(float a_money)
+    {
+        m_playersMoney += a_money;
+    }
+    /// <summary>
+    /// Function inside the PlayersBank.cs, used to decrease the amount of money in the players bank.
+    /// </summary>
+    /// <param name="a_money">Amount to takeaway from the players bank</param>
+    public void TakeAwayMoney(float a_money)
+    {
+        if ((m_playersMoney - a_money) < 0)
+        {
+            Debug.LogWarning("Warning: Player does not have enough currency to purchase item!");
+            return;
+        }
+        m_playersMoney -= a_money;
+    }
+    /// <summary>
+    /// Function inside the PlayersBank.cs, used to reset the amount of money in the players bank.
+    /// </summary>
+    public void ResetMoney()
+    {
+        m_playersMoney = 0;
+    }
+    /// <summary>
+    /// Function inside the PlayersBank.cs, used to set the players money to a specific amount.
+    /// </summary>
+    /// <param name="a_money">The specific amount to be set</param>
+    public void SetMoney(float a_money)
+    {
+        if (a_money < 0)
+        {
+            Debug.LogWarning("Warning: Player can not have minus currency!");
+            return;
+        }
+        m_playersMoney = a_money;
+    }
+    /// <summary>
+    /// Function inside the PlayersBank.cs, used to get the players current money.
+    /// </summary>
+    /// <returns>The int of the players currency is returned.</returns>
+    public float GetMoney()
+    {
+        return m_playersMoney;
+    }
+}
---
+++ Assets/Scripts/SB_Scripts/NewGame/UIManager2.cs
@@ -0,0 +1,26 @@
+﻿//////////////////////////////////////////////////
+/// File: UIManager2.cs
+/// Author: Sam Baker
+/// Date created: 27/02/20
+/// Last edit: 28/02/20
+/// Description: A new UI manger to handle elements on the canvas.
+/// Comments: 
+//////////////////////////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager2 : MonoBehaviour
+{
+    //////////////////////////////////////////////////
+    //// Variables
+    [SerializeField] private Text m_moneyText = null;
+    [SerializeField] private Color m_textColor = Color.black;
+
+    //////////////////////////////////////////////////
+    //// Functions
+    private void Start() => m_moneyText.color = m_textColor;
+
+    private void Update() => m_moneyText.text = PlayersBank.Instance.GetMoney().ToString();
+}
