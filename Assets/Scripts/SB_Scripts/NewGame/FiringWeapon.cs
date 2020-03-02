e6f04ac20dd28b76c5fea6a1ee01afa7d0b79149
Sam Baker <s1704633@connect.glos.ac.uk>
2020-03-02 00:24:52 +0000

    Firing weapon / Enemies

---
+++ Assets/Scripts/SB_Scripts/NewGame/FiringWeapon.cs
@@ -0,0 +1,94 @@
+﻿//////////////////////////////////////////////////
+/// File: FiringWeapon.cs
+/// Author: Sam Baker
+/// Date created: 1/03/20
+/// Last edit: 1/03/20
+/// Description: Script to control the firing off a weapon.
+/// Comments: 
+//////////////////////////////////////////////////
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Random = UnityEngine.Random;
+
+public class FiringWeapon : MonoBehaviour
+{
+    [Header(" > Weapon Parameters:")]
+    [Space(-10)]
+    [Header("_____________________________________________________")]
+    [Space(-20)]
+    [Header(" > Left Mouse Button.")]
+    [Space(-10)]
+    [Header("FiringWeapon's functions")]
+    //////////////////////////////////////////////////
+    //// Variables
+    [SerializeField] private bool m_weaponEnabled = true;
+    [SerializeField] private float m_fireRate = 0.2f;
+    private float m_timer = 0.0f;
+    [SerializeField] private float m_damage = 10.0f;
+    private float m_actaulDamage = 0.0f;
+    [SerializeField] [Range(0.01f, 0.99f)] private float m_dmgVariation = 0.2f;
+    private bool m_isFiring = false;
+    private GameObject m_cam = null;
+
+    //////////////////////////////////////////////////
+    //// Functions
+    private void Start() => m_cam = GameObject.Find("Sam'sTempCharacterController/PlayerOrientation/MainCamera");
+    private void Update() => ShouldFire();
+
+    private void ShouldFire()
+    {
+        if (m_isFiring)
+        {
+            if (m_weaponEnabled)
+            {
+                m_timer -= Time.deltaTime;
+                if(m_timer <= 0)
+                {
+                    //MUZZLE FLASH TRIGGERS HERE....................
+
+                    //Damage multiplier range
+                    m_actaulDamage = Random.Range(m_damage * ( 1 - m_dmgVariation), m_damage * (1 + m_dmgVariation));
+                    Debug.Log(m_actaulDamage);
+
+                    RaycastHit hit;
+                    if (Physics.Raycast(m_cam.transform.position, m_cam.transform.forward, out hit, 100.0f))
+                    {
+                        //Impact
+                        //Check to see if impacted object has health.
+                        if (hit.transform.gameObject.GetComponent<ObjectHealth>() != null)
+                        {
+                            hit.transform.gameObject.GetComponent<ObjectHealth>().TakeDamage(m_actaulDamage);
+                        }
+
+                    }
+                    m_timer = m_fireRate;
+                }
+            }
+        }
+    }
+
+    public void FireTrigger(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            if (m_isFiring)
+            {
+                m_isFiring = false;
+            }
+            else
+            {
+                m_isFiring = true;
+                if(m_weaponEnabled)
+                {
+                    m_timer = m_fireRate;
+                    PlayerZoom.Instance.SetZoom(true);
+                }
+            }
+        }
+    }
+
+    public void SetWeaponActive(bool a_true) => m_weaponEnabled = a_true;
+}
---
+++ Assets/Scripts/SB_Scripts/NewGame/ObjectHealth.cs
@@ -0,0 +1,73 @@
+﻿//////////////////////////////////////////////////
+/// File: ObjectHealth.cs
+/// Author: Sam Baker
+/// Date created: 1/03/20
+/// Last edit: 1/03/20
+/// Description: Script that can be placed on any object that can be destroyed.
+/// Comments: 
+//////////////////////////////////////////////////
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectHealth : MonoBehaviour
+{
+    [Header("Health Parameters:")]
+    [Space(-10)]
+    [Header("_____________________________________________________")]
+    [Space(-20)]
+    [Header("     can be destroyed.")]
+    [Space(-15)]
+    [Header(" > Can be placed on any object that")]
+    [Space(-10)]
+    [Header("ObjectHealth's functions")]
+    //////////////////////////////////////////////////
+    //// Variables
+    [SerializeField] private float m_startHealth = 100f;
+     private float m_currentHealth = 0f;
+    [SerializeField] private bool m_hasHealthBar = false;
+    private GameObject m_healthBar = null;
+
+    //////////////////////////////////////////////////
+    //// Functions
+    private void Start()
+    {
+        m_currentHealth = m_startHealth;
+        if(m_hasHealthBar)
+        {
+            m_healthBar = gameObject.transform.Find("Canvas").transform.Find("Health").gameObject;
+        }
+    }
+
+    private void Update()
+    {
+        if (m_currentHealth <= 0)
+        {
+            if (gameObject.transform.tag == "Player")
+            {
+                //Restart the scene
+                //     or whatever happens when the player dies..................
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void UpdateHealth()
+    {
+        m_healthBar.GetComponent<Image>().fillAmount = m_currentHealth / m_startHealth;
+    }
+
+    public void TakeDamage(float a_fvalue)
+    {
+        m_currentHealth -= a_fvalue;
+        if (m_hasHealthBar)
+        {
+            UpdateHealth();
+        }
+    }
+}
