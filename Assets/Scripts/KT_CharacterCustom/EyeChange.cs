3da744de34d033204a23add345bb51528a80e876
Kyle Tugwell <ktugwell@live.co.uk>
2020-03-05 12:00:47 +0000

    Eye Customisation now in

---
+++ Assets/Scripts/KT_CharacterCustom/EyeChange.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EyeChange : MonoBehaviour
+{
+
+    public Color eyeColour;
+    public Color32 currentColour;
+    public Texture currentTexture;
+    public GameObject charEyes;
+
+    void Update()
+    {
+        currentColour = this.GetComponent<Image>().color;
+        currentTexture = this.GetComponent<Image>().mainTexture;
+
+        //Debug.Log(currentColour.r + "" + currentColour.g + "" + currentColour.b);
+    }
+
+    public void ColourChange()
+    {
+        charEyes.GetComponent<Renderer>().material.color = currentColour;
+
+        Debug.Log("Button Pressed: Colour " + currentColour);
+    }
+
+    public void ImageChange()
+    {
+        charEyes.GetComponent<Renderer>().material.SetTexture("_MainTex", currentTexture);
+
+        Debug.Log("Button Pressed: Texture " + currentTexture.name);
+    }
+}
