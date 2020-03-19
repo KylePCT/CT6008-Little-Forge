bee267ac02f9ef8bc08d794c78c7004cde11ba24
BlondyMartinez <blondymartinezm@gmail.com>
2020-03-19 07:37:07 +0000

    xml loader/reader
    loads and reads a simple xml to print some dialogue lines on console when clicking the button

---
+++ Assets/Scripts/BM_Scripts/BM_DialogueSystem.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Xml;
+using UnityEngine;
+
+public class BM_DialogueSystem : MonoBehaviour {
+    [SerializeField] private string dialogueStoryName;
+
+    private XmlNodeList dialogueData;
+    
+    private int dialogueID = 0;
+    private int destinationID = 0;
+    public string dialogueLine { get; private set; }
+
+    private void Awake() {
+        dialogueData = BM_XmlLoader.LoadXMLdata();
+        Debug.Log(dialogueData);
+        dialogueData = dialogueData[1].SelectNodes(BM_Constants.Dialogue);
+    }
+
+    public void GetNextDialogue() {
+        string stringDialogueID = dialogueData[dialogueID].SelectSingleNode(BM_Constants.XmlDialogueID).Value;
+        dialogueID =  int.Parse(stringDialogueID);
+
+
+        string stringDestinationID = dialogueData[dialogueID].FirstChild.SelectSingleNode(BM_Constants.XmlDialogueDestinationID).Value;
+        destinationID = int.Parse(stringDestinationID);
+
+        dialogueLine = dialogueData[dialogueID].SelectSingleNode(BM_Constants.Line).InnerText;
+
+        Debug.Log(dialogueLine);
+
+        dialogueID = destinationID;
+    }
+}
