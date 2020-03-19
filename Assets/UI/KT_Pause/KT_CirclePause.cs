fbe028200bc1590793d0a29c84bab2e1d6d1ea8d
Kyle Tugwell <ktugwell@live.co.uk>
2020-03-19 14:57:54 +0000

    Addind new UI and tweening capabiltiies

---
+++ Assets/UI/KT_Pause/KT_CirclePause.cs
@@ -0,0 +1,117 @@
+﻿//////////////////////////////////////////////////
+/// File: KT_CirclePause.cs
+/// Author: Kyle Tugwell
+/// Date created: 19/03/20
+/// Last edit: 19/03/20
+/// Description: A script to manage a circular pause menu which detects the mouse position to decide which wedge is activated.
+/// Comments: 
+//////////////////////////////////////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KT_CirclePause : MonoBehaviour
+{
+    //////////////////////////////////////////////////
+    //// Variables
+    public List<MenuButton> buttons = new List<MenuButton>();
+    private Vector2 mousePos;
+    private Vector2 fromVector2M = new Vector2(0.5f, 1.0f);
+    private Vector2 centerCircle = new Vector2(0.5f, 0.5f);
+    private Vector2 toVector2M;
+
+    public int menuItems;
+    public int currentMenuItem;
+    private int oldMenuItem;
+
+    //////////////////////////////////////////////////
+    //// Functions
+    void Start()
+    {
+        //detect menuItems through numbers of buttons
+        menuItems = buttons.Count; 
+
+        //set default colour for all buttons
+        foreach(MenuButton button in buttons)
+        {
+            button.sceneImage.color = button.idleColour;
+        }
+
+        currentMenuItem = 0;
+        oldMenuItem = 0;
+    }
+
+    void Update()
+    {
+        //get the currentMenuItem through moust position
+        getCurrentMenuItem();
+
+        //click to do a thing on the menu
+        if(Input.GetMouseButtonDown(0))
+        {
+            buttonAction();
+        }
+    }
+
+    public void getCurrentMenuItem()
+    {
+        //get mousePosition through inputs and toVector2M by allocating position to screen resolution
+        mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        toVector2M = new Vector2(mousePos.x / Screen.width, mousePos.y / Screen.height);
+
+        //where on the mouse is the screen, determine the angle and then convert it to degrees from radians
+        float mouseAngle = (Mathf.Atan2(fromVector2M.y - centerCircle.y, fromVector2M.x - centerCircle.x) - Mathf.Atan2(toVector2M.y - centerCircle.y, toVector2M.x - centerCircle.x)) * Mathf.Rad2Deg;
+
+        //make sure the angle does not go below 0 e.g. converts -90 to 270
+        if (mouseAngle < 0)
+        {
+            mouseAngle += 360;
+        }
+
+        //shows which wedge is selected through the angle
+        currentMenuItem = (int)(mouseAngle / (360 / menuItems));
+
+        //change colours to show feedback dependant on highlight
+        if (currentMenuItem != oldMenuItem)
+        {
+            buttons[oldMenuItem].sceneImage.color = buttons[oldMenuItem].idleColour;
+            oldMenuItem = currentMenuItem;
+            buttons[currentMenuItem].sceneImage.color = buttons[currentMenuItem].highlightColour;
+        }
+    }
+
+    //when buttons are pressed
+    public void buttonAction()
+    {
+        buttons[currentMenuItem].sceneImage.color = buttons[currentMenuItem].downColour;
+
+        //wedge actions
+        if(currentMenuItem == 0)
+        {
+            Debug.Log("0 TR pressed");
+        }
+
+        else if(currentMenuItem == 1)
+        {
+            Debug.Log("1 M pressed");
+        }        
+        
+        else if(currentMenuItem == 2)
+        {
+            Debug.Log("2 TL pressed");
+        }
+    }
+}
+
+//fields for colours and images which allow them to work in the list for added functionality and efficiency
+[System.Serializable]
+public class MenuButton
+{
+    public string name;
+    public Image sceneImage;
+    public Color idleColour = Color.white;
+    public Color highlightColour = Color.grey;
+    public Color downColour = Color.gray;
+}
---
+++ Assets/UI/KT_Pause/KT_Pause.cs
@@ -0,0 +1,161 @@
+﻿//////////////////////////////////////////////////
+/// File: KT_Pause.cs
+/// Author: Kyle Tugwell
+/// Date created: 19/03/20
+/// Last edit: 19/03/20
+/// Description: A script to manage pausing and the menu's animations.
+/// Comments: Fading code attained from User: MichaelHouse at https://gamedev.stackexchange.com/questions/123938/unity-i-want-to-make-my-ui-text-fade-in-after-5-seconds on 19/03/2020 13:19; everything else is written by me
+//////////////////////////////////////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KT_Pause : MonoBehaviour
+{
+    //////////////////////////////////////////////////
+    //// Variables
+    public bool isPaused = false;
+    public GameObject pauseCanvas;
+    public GameObject mainCanvas;
+
+    //each menu wedge
+    public GameObject wedge_1;
+    public GameObject wedge_2;
+    public GameObject wedge_3;
+
+    //cG is for alphas
+    public CanvasGroup canvasGroup;
+
+    //fade for alpha
+    public float fadeInDelay;
+
+    //////////////////////////////////////////////////
+    //// Functions
+    private void Start()
+    {
+        //make sure all tweens are scaled to 0 and alphas are 0 for the pause menu
+        LeanTween.scale(wedge_1, new Vector3(0, 0, 0), 0f);
+        LeanTween.scale(wedge_2, new Vector3(0, 0, 0), 0f);
+        LeanTween.scale(wedge_3, new Vector3(0, 0, 0), 0f);
+
+        canvasGroup.alpha = 0;
+    }
+    void Update()
+    {
+        //if 'Esc' or button 'Cancel is pressed
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (isPaused == false)
+            {
+                //show cursor, swap canvases
+                isPaused = true;
+                Cursor.visible = true;
+                pauseCanvas.SetActive(true);
+                mainCanvas.SetActive(false);
+
+                //have wedges zoom in
+                LeanTween.scale(wedge_1, new Vector3(1, 1, 1), 0.2f);
+                LeanTween.scale(wedge_2, new Vector3(1, 1, 1), 0.2f);
+                LeanTween.scale(wedge_3, new Vector3(1, 1, 1), 0.2f);
+
+                //fade
+                FadeIn();
+            }
+
+            else
+            {
+                //if it's already paused, do the opposite
+                isPaused = false;
+                Cursor.visible = false;
+
+                LeanTween.scale(wedge_1, new Vector3(0, 0, 0), 0.2f).setOnComplete(mainCanvHidden);
+                LeanTween.scale(wedge_2, new Vector3(0, 0, 0), 0.2f).setOnComplete(mainCanvHidden);
+                LeanTween.scale(wedge_3, new Vector3(0, 0, 0), 0.2f).setOnComplete(mainCanvHidden);
+
+                FadeOut();
+            }
+        }
+    }
+
+    public void mainCanvHidden()
+    {
+        pauseCanvas.SetActive(false);
+        mainCanvas.SetActive(true);
+    }
+
+    //this function is repeated for buttons to be able to call it
+    public void closePause()
+    {
+        isPaused = false;
+        Cursor.visible = false;
+
+        LeanTween.scale(wedge_1, new Vector3(0, 0, 0), 0.2f).setOnComplete(mainCanvHidden);
+        LeanTween.scale(wedge_2, new Vector3(0, 0, 0), 0.2f).setOnComplete(mainCanvHidden);
+        LeanTween.scale(wedge_3, new Vector3(0, 0, 0), 0.2f).setOnComplete(mainCanvHidden);
+
+        FadeOut();
+    }
+
+    //Fading code attained from User: MichaelHouse at https://gamedev.stackexchange.com/questions/123938/unity-i-want-to-make-my-ui-text-fade-in-after-5-seconds on 19/03/2020 13:19
+
+    //////////////////////////////////////////////////
+    //// Variables
+    public float changeTimeSeconds = 5;
+    public float startAlpha = 0;
+    public float endAlpha = 1;
+
+    float changeRate = 0;
+    float timeSoFar = 0;
+    bool fading = false;
+
+    //////////////////////////////////////////////////
+    //// Functions
+    public void FadeIn()
+    {
+        startAlpha = 0;
+        endAlpha = 1;
+        timeSoFar = 0;
+        fading = true;
+        StartCoroutine(FadeCoroutine());
+    }
+
+    public void FadeOut()
+    {
+        startAlpha = 1;
+        endAlpha = 0;
+        timeSoFar = 0;
+        fading = true;
+        StartCoroutine(FadeCoroutine());
+    }
+
+    //runs the fade
+    IEnumerator FadeCoroutine()
+    {
+        changeRate = (endAlpha - startAlpha) / changeTimeSeconds;
+        SetAlpha(startAlpha);
+        while (fading)
+        {
+            timeSoFar += Time.deltaTime;
+
+            if (timeSoFar > changeTimeSeconds)
+            {
+                fading = false;
+                SetAlpha(endAlpha);
+                yield break;
+            }
+            else
+            {
+                SetAlpha(canvasGroup.alpha + (changeRate * Time.deltaTime));
+            }
+
+            yield return null;
+        }
+    }
+
+    //sets alpha for the group
+    public void SetAlpha(float alpha)
+    {
+        canvasGroup.alpha = Mathf.Clamp(alpha, 0, 1);
+    }
+}
