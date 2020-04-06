0786e5eb31a7b50176b844b47109bcc39d7c1833
Kyle Tugwell <ktugwell@live.co.uk>
2020-04-06 13:39:14 +0100

    Finally fixed char customisation
    Added a script which forces mouse to stay active
    Disabled player input within the scene

--- Assets/Scripts/KT_CharacterCustom/ShowMouse.cs
+++ Assets/Scripts/KT_CharacterCustom/ShowMouse.cs
@@ -5,14 +5,9 @@ using UnityEngine;
 public class ShowMouse : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
     }
 }
