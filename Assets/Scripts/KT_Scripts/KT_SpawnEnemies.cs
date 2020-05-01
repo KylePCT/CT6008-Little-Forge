4754c4fda0650616d4e7b7d88984eec0a658bdd8
Sam Baker <s1704633@connect.glos.ac.uk>
2020-05-02 00:32:36 +0100

    Stopped enemies spawning in objects

--- Assets/Scripts/KT_Scripts/KT_SpawnEnemies.cs
+++ Assets/Scripts/KT_Scripts/KT_SpawnEnemies.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class KT_SpawnEnemies : MonoBehaviour
 {
@@ -18,6 +19,8 @@ public class KT_SpawnEnemies : MonoBehaviour
 
     private GameObject Player;
 
+    private Vector3 m_randPointOnMesh = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,14 +35,34 @@ public class KT_SpawnEnemies : MonoBehaviour
     {
         while (enemyCount < enemySpawnCount)
         {
-            xPos = Random.Range(405, 280);
-            zPos = Random.Range(70, 10);
+            //xPos = Random.Range(405, 280);
+            //zPos = Random.Range(70, 10);
 
-            Instantiate(enemies[UnityEngine.Random.Range(0, enemies.Length - 1)], new Vector3(xPos, yHeight, zPos), Quaternion.identity);
+            //Instantiate(enemies[UnityEngine.Random.Range(0, enemies.Length - 1)], new Vector3(xPos, yHeight, zPos), Quaternion.identity);
+            
+            //Edited to stop enemies spawning inside of objects - Sam Baker
+            m_randPointOnMesh = RandomNavSphere(new Vector3(345f, 67f, 23f), 999f, -1);
+            Instantiate(enemies[UnityEngine.Random.Range(0, enemies.Length - 1)], m_randPointOnMesh, Quaternion.identity);
             Instantiate(poofVFX, new Vector3(xPos, yHeight, zPos), Quaternion.identity);
             Debug.Log("Enemy " + enemyCount + " spawned.");
             yield return new WaitForSeconds(5f);
             enemyCount += 1;
         }
+    }
+    /// <summary>
+    /// Function used to get a random point on the navmesh, gets a random point within a sphere. - Sam Baker
+    /// </summary>
+    /// <param name="a_origin">Center of the sphere.</param>
+    /// <param name="a_dist">Radius of the sphere.</param>
+    /// <param name="a_layermask"></param>
+    /// <returns>Return a Vector3.</returns>
+    public static Vector3 RandomNavSphere(Vector3 a_origin, float a_dist, int a_layermask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * a_dist;
+        randDirection += a_origin;
+        NavMeshHit navHit;
+        NavMesh.SamplePosition(randDirection, out navHit, a_dist, a_layermask);
+
+        return navHit.position;
     }
 }
