e14416f26f5fe6c525a75000bdcf1d993dca1dc6
Sam Baker <s1704633@connect.glos.ac.uk>
2020-05-03 00:55:31 +0100

    Fixed scenes / added delete char button

---
+++ Assets/Scripts/SB_Scripts/NewGame/DeleteCharacters.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeleteCharacters : MonoBehaviour
+{
+    public void DeleteCharactersButton()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            SaveGameManager.DeleteCharacter(i);
+        }
+        SceneManager.LoadScene(0);
+    }
+}
