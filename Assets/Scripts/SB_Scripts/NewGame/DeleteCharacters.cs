093209b80d4b3263017d72d48ac3d71cf91d7c6a
Sam Baker <s1704633@connect.glos.ac.uk>
2020-05-03 02:41:40 +0100

    comments

--- Assets/Scripts/SB_Scripts/NewGame/DeleteCharacters.cs
+++ Assets/Scripts/SB_Scripts/NewGame/DeleteCharacters.cs
@@ -1,10 +1,18 @@
-﻿using System.Collections;
+﻿//////////////////////////////////////////////////
+/// File: DeleteCharacters.cs
+/// Author: Sam Baker
+/// Date Created: 02/05/20
+/// Description: 
+//////////////////////////////////////////////////
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class DeleteCharacters : MonoBehaviour
 {
+    //////////////////////////////////////////////////
+    //// Functions
     public void DeleteCharactersButton()
     {
         for (int i = 0; i < 4; i++)
