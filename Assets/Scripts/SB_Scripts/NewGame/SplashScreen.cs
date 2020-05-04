49024fa352cd6e6b3de66c3ab60a0a9746178928
Sam Baker <s1704633@connect.glos.ac.uk>
2020-05-04 01:41:34 +0100

    Little splash screen on the menu

---
+++ Assets/Scripts/SB_Scripts/NewGame/SplashScreen.cs
@@ -0,0 +1,18 @@
+﻿//////////////////////////////////////////////////
+// File: SplashScreen.cs
+// Author: Sam Baker
+// Date Created: 04/05/20
+// Description: Simple script used to call a function at the end of an animation
+//////////////////////////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashScreen : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        //Hide this UI
+        gameObject.SetActive(false);
+    }
+}
