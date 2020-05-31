e820da8870799484bd6134a38d13a392c5e261b8
Kyle Tugwell <ktugwell@live.co.uk>
2020-05-31 16:43:12 +0100

    Environment edits and adding in Kirils assets
    + extra animations

---
+++ Assets/Scripts/KT_Scripts/KT_Bobbing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Code retrieved from https://forum.unity.com/threads/trying-to-simulate-a-ships-rocking-motion-in-the-ocean.465557/ 31/05 16:25
+//Adapted for purpose.
+public class KT_Bobbing : MonoBehaviour
+{
+    public float originalY = 67f;
+    public float bobbingMultiplier = .5f;
+
+// Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, originalY + ((float)Math.Sin(Time.time) * bobbingMultiplier), transform.position.z);
+    }
+}
---
+++ Assets/Shaders/KT_depthMask.shader
@@ -0,0 +1,22 @@
+﻿Shader "Custom/KT_depthMask"
+
+//Retrieved from http://wiki.unity3d.com/index.php/DepthMask?_ga=2.49168550.1086181043.1590874499-1439766605.1584272748 31/05 16:32
+
+{
+
+	SubShader{
+		// Render the mask after regular geometry, but before masked geometry and
+		// transparent things.
+
+		Tags {"Queue" = "Geometry+10" }
+
+		// Don't draw in the RGBA channels; just the depth buffer
+
+		ColorMask 0
+		ZWrite On
+
+		// Do nothing specific in the pass:
+
+		Pass {}
+	}
+}
